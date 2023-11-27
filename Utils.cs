1f26f17f4f390b3f4816fe852aaebe6e6d9c7f04
JorgeCarranza04 <115588474+JorgeCarranza04@users.noreply.github.com>
2023-11-26 21:39:24 -0600

    Add files via upload

---
+++ Utils.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App_Cursos
+{
+	public class Utils
+	{
+		public static string ImageToByte(string path)
+		{
+			try
+			{
+				byte[] img = System.IO.File.ReadAllBytes(path);
+				string base64String = Convert.ToBase64String(img);
+				return base64String;
+
+			}
+			catch (Exception x)
+			{
+				throw new Exception(x.Message);
+			}
+		}
+    }
+}
+
+
+
+
